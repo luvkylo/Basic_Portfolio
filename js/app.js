785b867c81c71f3e2f43acdfeb6d6b8cedfdb641
Alvin <alvinhewittalvin@gmail.com>
2019-05-17 13:12:23 -0700

    carousel scroll bar fixed

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-    $('.carousel').carousel();
+  $('.carousel').carousel();
 
-    $('.carousel.carousel-slider').carousel({
-	    fullWidth: true,
-	    indicators: true
-	});
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
 
-    $('.moveNextCarousel').click(function(e){
-      e.preventDefault();
-      e.stopPropagation();
-      $('.carousel').carousel('next');
-   	});
+  $('.moveNextCarousel').click(function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    $('.carousel').carousel('next');
+ 	});
 
-   	// move prev carousel
-   	$('.movePrevCarousel').click(function(e){
-      e.preventDefault();
-      e.stopPropagation();
-      $('.carousel').carousel('prev');
-   	});
+ 	// move prev carousel
+ 	$('.movePrevCarousel').click(function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    $('.carousel').carousel('prev');
+ 	});
 
-   	$( "a.nav" ).click(function( event ) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
-    });
+ 	$( "a.nav" ).click(function( event ) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+  });
 });
